dc5ac943f7f50d2907d2d293e0174723a65fd5b6
taro-28 <tarotaro0208@gmail.com>
2023-10-14 00:55:31 +0900

    implement add todo in sc page

--- src/app/server-components/page.tsx
+++ src/app/server-components/page.tsx
@@ -1,28 +1,78 @@
 import { PageTitle } from '@/components/PageTitle'
 import { gqlFetch } from '../../functions/gqlFetch'
 import { Todo } from '../../types/todo'
+import { Fragment } from 'react'
+import { Label } from '@/components/Label'
+import { Input } from '@/components/Input'
+import { Button } from '@/components/Button'
+import { revalidatePath } from 'next/cache'
 
-export default async function ServerComponents() {
-  const todos: Todo[] = await gqlFetch(`
-  query feedTodo {
-    todos {
+const todosQuery = `query { 
+  todos {
+    id
+    text
+    user {
       id
-      text
-      user {
-        id
-      }
+      name
     }
   }
-  `).then(({ data: { todos } }) => todos)
+}
+`
+
+const createTodoMutation = `mutation ($text: String! $userId: String!) {
+  createTodo(input: {
+    text: $text,
+    userId: $userId
+  }){
+    id
+    text
+    done
+    user {
+      id
+      name
+    }
+  }
+}
+`
+
+export default async function ServerComponents() {
+  const todos: Todo[] = await gqlFetch({
+    query: todosQuery,
+  }).then(({ data: { todos } }) => todos)
+
+  const handleCreateTodo = async (formData: FormData) => {
+    'use server'
+    await gqlFetch({
+      query: createTodoMutation,
+      variables: { text: formData.get('text'), userId: formData.get('userId') },
+    })
+    revalidatePath('/server-components')
+  }
 
   return (
     <div className='space-y-4'>
       <PageTitle>Server Components</PageTitle>
-      {todos.map((todo) => (
-        <div key={todo.id}>
-          <p>{todo.text}</p>
-        </div>
-      ))}
+      <form action={handleCreateTodo} className='flex space-x-2'>
+        <Label>
+          Text
+          <Input className='ml-1' name='text' />
+        </Label>
+        <Label>
+          UserID
+          <Input className='ml-1' name='userId' />
+        </Label>
+        <Button type='submit'>Create</Button>
+      </form>
+      <div className='grid grid-cols-2 gap-2'>
+        <div className='font-semibold'>Text</div>
+        <div className='font-semibold'>UserID</div>
+        {todos.map((todo) => (
+          <Fragment key={todo.id}>
+            <div>{todo.text}</div>
+            <div>{todo.user.name}</div>
+          </Fragment>
+        ))}
+      </div>
     </div>
   )
 }

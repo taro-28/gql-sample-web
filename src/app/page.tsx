dc1a720b5f5b7ff1349b76454fca70999053f975
taro-28 <tarotaro0208@gmail.com>
2023-10-22 19:03:39 +0900

    add todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+import { Card } from '@/components/Card'
 import { PageTitle } from '@/components/PageTitle'
+
+const todos = [
+  'return types',
+  'create company detail query',
+  'check about fetch policy in sc',
+  'typed document node',
+]
 
 export default function Home() {
   return (
     <div className='w-full space-y-4'>
-      <PageTitle>Home</PageTitle>
+      <PageTitle>Todo</PageTitle>
+      <div className='grid grid-cols-3 gap-4'>
+        {todos.map((todo) => (
+          <Card content={todo} key={todo} title='' />
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export const Card = ({ title, content, className, ...props }: Props) => {
   return (
-    <div className={clsx(className, 'space-y-2 rounded-md border-2 p-4')} {...props}>
+    <div className={clsx(className, 'space-y-2 rounded-md border-2 p-4 shadow-md')} {...props}>
       <div className='text-xl'>{title}</div>
       <div className='text-2xl font-bold'>{content}</div>
     </div>
